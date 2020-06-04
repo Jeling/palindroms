9c2aeadcc9d8957f497858629b4c90ac9eb40084
Krzysztof Klamycki <krzkamkla@gmail.com>
2020-06-04 13:04:20 +0200

    Adding regex

--- palindroms.py
+++ palindroms.py
@@ -1,14 +1,22 @@
+import re
+
 def is_palindrome(word): 
     
-    for i in range(0, int(len(word)/2)):  
-        if word[i] != word[len(word)-i-1]: 
+    text = re.sub('[^a-z]+', '', word.lower())
+    for i in range(0, int(len(text)/2)):  
+        if text[i] != text[len(text)-i-1]: 
             return False
     return True
+
+    #for i in range(0, int(len(word)/2)):  
+        #if word[i] != word[len(word)-i-1]: 
+            #return False
+    #return True
     
 word_to_check = input("Podaj słowo do sprawdzenia\n")
 ans = is_palindrome(word_to_check) 
 
 if (ans): 
-    print(f"""Tak, słowo {word_to_check} to palindrom""") 
+    print(f"""Tak, {word_to_check} to palindrom""") 
 else: 
-    print(f"""Nie, słowo {word_to_check} to nie palindrom""") 
+    print(f"""Nie, {word_to_check} to nie palindrom""") 
